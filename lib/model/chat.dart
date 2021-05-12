9260694acaf9ceefc874dfa2762202fb52933713
Rishabh <rishabhmishra23599@gmail.com>
2021-05-12 22:24:01 +0530

    added models

---
+++ lib/model/chat.dart
@@ -0,0 +1,18 @@
+/*
+TODO:
+sender name
+sender_unique_id
+message
+timestamp
+imagrURL
+emoji
+is_chat_of_group
+*/
+
+class Chat {
+  String senderName;
+  String senderUID;
+  String message;
+  String timestamp;
+// String imagrURL
+}
