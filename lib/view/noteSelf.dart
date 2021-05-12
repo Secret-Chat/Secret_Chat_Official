9578fa55acc612c7a16d45e9c2e1575d5a79e0fb
manavparikh01 <manavparikh01@gmail.com>
2021-05-12 23:31:56 +0530

    Updated Note To Self Page

--- lib/view/noteSelf.dart
+++ lib/view/noteSelf.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NoteSelf extends StatefulWidget {
   @override
@@ -6,6 +8,10 @@ class NoteSelf extends StatefulWidget {
 }
 
 class _NoteSelfState extends State<NoteSelf> {
+  final _auth = FirebaseAuth.instance;
+  CollectionReference stream = FirebaseFirestore.instance
+      .collection('users/FlcIcaaSnmc4DFWDTBXgUuruxI22/messages');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +25,42 @@ class _NoteSelfState extends State<NoteSelf> {
           ),
         ),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: stream.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+
+          return new ListView(
+            children: snapshot.data.docs.map((DocumentSnapshot document) {
+              return new ListTile(
+                title: new Text(document.data()['text']),
+                //subtitle: new Text(document.data()['company']),
+              );
+            }).toList(),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            // Firestore.instance
+            //     .collection('chat/G8Rk0dGURGZna9CdyyNf/messages')
+            //     .snapshots()
+            //     .listen((data) {
+            //   data.documents.forEach((document) {
+            //     print(
+            //       document['text'],
+            //     );
+            FirebaseFirestore.instance
+                .collection('users/FlcIcaaSnmc4DFWDTBXgUuruxI22/messages')
+                .add({'text': 'This is another another another message'});
+          }),
     );
   }
 }
