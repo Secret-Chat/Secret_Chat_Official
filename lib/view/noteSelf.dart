94bdc84d7f577b2e32aacf1dea142d04e08671d1
manavparikh01 <manavparikh01@gmail.com>
2021-05-13 22:20:00 +0530

    Update noteSelf.dart

--- lib/view/noteSelf.dart
+++ lib/view/noteSelf.dart
@@ -9,8 +9,12 @@ class NoteSelf extends StatefulWidget {
 
 class _NoteSelfState extends State<NoteSelf> {
   final _auth = FirebaseAuth.instance;
+  final _titleController = TextEditingController();
+
   CollectionReference stream = FirebaseFirestore.instance
-      .collection('users/FlcIcaaSnmc4DFWDTBXgUuruxI22/messages');
+      .collection('users/FlcIcaaSnmc4DFWDTBXgUuruxI22/messages')
+      .orderBy('text', descending: false);
+  //.orderBy('createdOn', descending: true);
 
   @override
   Widget build(BuildContext context) {
@@ -25,42 +29,104 @@ class _NoteSelfState extends State<NoteSelf> {
           ),
         ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: stream.snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 68,
+          child: Column(
+            children: <Widget>[
+              Container(
+                color: Color.fromRGBO(200, 200, 200, 0.3),
+                height: MediaQuery.of(context).size.height - 250,
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: stream.snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Loading");
-          }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Text("Loading");
+                    }
 
-          return new ListView(
-            children: snapshot.data.docs.map((DocumentSnapshot document) {
-              return new ListTile(
-                title: new Text(document.data()['text']),
-                //subtitle: new Text(document.data()['company']),
-              );
-            }).toList(),
-          );
-        },
+                    return new ListView(
+                      reverse: false,
+                      children:
+                          snapshot.data.docs.map((DocumentSnapshot document) {
+                        return new ListTile(
+                          title: new Text(document.data()['text']),
+                          //subtitle: new Text(document.data()['company']),
+                        );
+                      }).toList(),
+                    );
+                  },
+                ),
+              ),
+              Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width,
+                color: Color.fromRGBO(23, 234, 34, 0.2),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 98,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 2,
+                            color: Colors.black87,
+                            style: BorderStyle.solid),
+                      ),
+                      width: MediaQuery.of(context).size.width - 100,
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: _titleController,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.send,
+                        color: Colors.black,
+                        size: 25,
+                      ),
+                      onPressed: () {
+                        // ignore: prefer_is_not_empty
+                        if (_titleController.text.isNotEmpty) {
+                          FirebaseFirestore.instance
+                              .collection(
+                                  'users/FlcIcaaSnmc4DFWDTBXgUuruxI22/messages')
+                              .add({
+                            'text': _titleController.text,
+                            'createdOn': Timestamp.now(),
+                          });
+                        }
+                        _titleController.text = '';
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            // Firestore.instance
-            //     .collection('chat/G8Rk0dGURGZna9CdyyNf/messages')
-            //     .snapshots()
-            //     .listen((data) {
-            //   data.documents.forEach((document) {
-            //     print(
-            //       document['text'],
-            //     );
-            FirebaseFirestore.instance
-                .collection('users/FlcIcaaSnmc4DFWDTBXgUuruxI22/messages')
-                .add({'text': 'This is another another another message'});
-          }),
+      // floatingActionButton: FloatingActionButton(
+      //     child: Icon(Icons.add),
+      //     onPressed: () {
+      //       // Firestore.instance
+      //       //     .collection('chat/G8Rk0dGURGZna9CdyyNf/messages')
+      //       //     .snapshots()
+      //       //     .listen((data) {
+      //       //   data.documents.forEach((document) {
+      //       //     print(
+      //       //       document['text'],
+      //       //     );
+      //       FirebaseFirestore.instance
+      //           .collection('users/FlcIcaaSnmc4DFWDTBXgUuruxI22/messages')
+      //           .add({'text': 'This is another another another message'});
+      //     }),
     );
   }
 }
