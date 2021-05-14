f53b0a47a0fc5584110144e3c0c0ff18b634eac6
manavparikh01 <manavparikh01@gmail.com>
2021-05-14 18:15:27 +0530

    Search Page

--- lib/view/searchPage.dart
+++ lib/view/searchPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:secretchat/controller/auth_controller.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -6,11 +10,73 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final _titleController = TextEditingController();
+
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Search Page'),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height - 68,
+        child: Expanded(
+            child: Container(
+          height: MediaQuery.of(context).size.height - 68,
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: TextField(
+                          decoration:
+                              InputDecoration(labelText: 'Search by Email'),
+                          controller: _titleController,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.send,
+                        color: Colors.black,
+                        size: 25,
+                      ),
+                      onPressed: () {
+                        // ignore: prefer_is_not_empty
+                        // if (_titleController.text.isNotEmpty) {
+                        //   FirebaseFirestore.instance
+                        //       .collection(
+                        //           'users/${getxController.authData}/messages')
+                        //       .add({
+                        //     'text': _titleController.text,
+                        //     'createdOn': Timestamp.now(),
+                        //   });
+                        //   getxController.printer();
+                        // }
+                        _titleController.text = '';
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                  child: Container(
+                child: ListTile(),
+              ))
+            ],
+          ),
+        )),
       ),
     );
   }
